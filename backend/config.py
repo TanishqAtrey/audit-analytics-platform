d03841e238da978ef69bfc4f1503bf39e84761f0
Tanishq Atrey <tanishqatrey@gmail.com>
2026-09-23 21:24:50 +0530

    updated readme

--- backend/config.py
+++ backend/config.py
@@ -85,6 +85,4 @@ class Settings(BaseSettings):
     model_config = SettingsConfigDict(env_file=".env", env_file_encoding="utf-8", extra="ignore")
 
 
-@lru_cache
-def get_settings() -> Settings:
-    return Settings()
+@lru_cach
